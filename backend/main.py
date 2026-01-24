c21308ef2e3061ecb26d5024c62c82cc094975eb
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-01-24 16:11:43 +0200

    configure FastAPI application title using `settings.PROJECT_NAME`.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
-app = FastAPI(title="Banking Processing API", version="0.1.0")
+app = FastAPI(title=settings.PROJECT_NAME, version="0.1.0")
 
 @app.get("/")
 def read_root():
